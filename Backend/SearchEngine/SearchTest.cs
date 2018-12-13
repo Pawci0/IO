ea461d6f9d146fdaccc9f029cd846f6f62e3ff38
Krzysztof Barden <wiedzmenpl@gmail.com>
2018-12-14 00:19:54 +0100

    Fuzzy search foundations

---
+++ Backend/SearchEngine/SearchTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static SearchEngine.Search;
+
+namespace SearchEngine
+{
+    [TestClass]
+    public class SearchTest
+    {
+        [TestMethod]
+        public void FuzzySearchTest()
+        {
+            var myList = new List<string>{"ala ma kota", "element"};
+
+            var results = from value in myList
+                where ContainsFuzzy(value, "kot")
+                select value;
+
+            Assert.AreEqual(1, results.Count());
+        }
+
+        [TestMethod]
+        public void LevenshteinTest()
+        { 
+            Assert.AreEqual(6, LevenshteinDistance("element", "kot"));
+        }
+    }
+}
