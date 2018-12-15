59e3e044bf912d1b1aa78a58eb96620646746c50
Krzysztof Barden <wiedzmenpl@gmail.com>
2018-12-15 19:54:26 +0100

    rest of classes

---
+++ Backend/SearchEngine/RatingSearch.cs
@@ -0,0 +1,42 @@
+﻿using Database;
+using SearchEngine.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngine
+{
+    internal class RatingSearch : ProductSearch
+    {
+
+        public override void Search(string phrase, SortTypeEnum sortType, Dictionary<string, string> filters)
+        {
+            IEnumerable<Product> results = from Product product in dataContext.GetAllProducts()
+                                           where AverageRatings(product.Ratings).ToString().ContainsFuzzy(phrase)
+                                           select product;
+
+            if (searchResult is null)
+            {
+                searchResult = OrderBy(results, sortType).ToList();
+            }
+            else
+            {
+                searchResult.AddRange(OrderBy(results, sortType));
+            }
+        }
+
+        private double AverageRatings(ICollection < Database.Rating > ratings)
+        {
+            double avg = 0;
+            foreach (var value in ratings)
+            {
+                avg += value.Value;
+            }
+
+            return Math.Round(avg/ratings.Count, 2);
+        }
+
+    }
+}
---
+++ Backend/SearchEngine/TagSearch.cs
@@ -0,0 +1,30 @@
+﻿using Database;
+using SearchEngine.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngine
+{
+    internal class TagSearch : ProductSearch
+    {
+        public override void Search(string phrase, SortTypeEnum sortType, Dictionary<string, string> filters)
+        {
+            IEnumerable<Product> results = from Product product in dataContext.GetAllProducts()
+                                           from Tag tag in product.Tags
+                                           where tag.Name.ContainsFuzzy(phrase)
+                                           select product;
+
+            if (searchResult is null)
+            {
+                searchResult = OrderBy(results, sortType).ToList();
+            }
+            else
+            {
+                searchResult.AddRange(OrderBy(results, sortType));
+            }
+        }
+    }
+}
