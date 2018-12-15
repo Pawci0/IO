74ed115b4708506771116c306658d939a4bc9940
Szymon <szymonicho@gmail.com>
2018-12-15 19:43:22 +0100

    initial work on recommendation engine

---
+++ Backend/Recommendation/RandomRecommendation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Database;
+
+namespace Recommendation
+{
+    class RandomRecommendation : IRecommendation
+    {
+        private DBManager db;
+        public RandomRecommendation(DBManager db)
+        {
+            this.db = db;
+        }
+
+        public IEnumerable<Product> GetRecommendedProducts(int userId, int amount)
+        {
+            return db.GetAllProducts().Shuffle().Take(amount);
+        }
+    }
+}
---
+++ Backend/Recommendation/RecommendationEngine.cs
@@ -0,0 +1,36 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recommendation
+{
+    public class RecommendationEngine : IRecommendation
+    {
+        private DBManager db;
+        public RecommendationEngine()
+        {
+            db = new DBManager();
+        }
+
+        public IEnumerable<Product> GetRecommendedProducts(int userId, int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IRecommendation SelectEngine()
+        {            
+            var ratings = db.GetAllRatings();
+            if(ratings.Count == 0)
+            {
+                return new RandomRecommendation(db);
+            }
+            else
+            {
+                // personalized recommendation engine
+            }
+        }
+    }
+}
