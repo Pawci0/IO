8f8f35631a34e264fe2d7257f45cc6612fffb70e
Szymon <szymonicho@gmail.com>
2018-12-16 13:14:22 +0100

    typo

--- Backend/RecommendationTests/RecommendationEngineTests.cs
+++ Backend/RecommendationTests/RecommendationEngineTests.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Recommendation;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Moq;
 using Database;
 
