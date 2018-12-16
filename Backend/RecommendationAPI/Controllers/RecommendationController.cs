f7c64ff11d2cab6779cd671f6a77d303dfe435f7
Szymon <szymonicho@gmail.com>
2018-12-16 19:59:41 +0100

    removed unused imports

--- Backend/RecommendationAPI/Controllers/RecommendationController.cs
+++ Backend/RecommendationAPI/Controllers/RecommendationController.cs
@@ -1,10 +1,6 @@
 ﻿using Database;
 using Recommendation;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace RecommendationAPI.Controllers
