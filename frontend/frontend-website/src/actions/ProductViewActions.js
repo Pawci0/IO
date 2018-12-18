740206d04462b636040afe7dca11aaa8cc924c5e
Wiktor <wiktor.muraszko@wp.pl>
2018-12-18 19:18:10 +0100

    datamoc

--- frontend/frontend-website/src/actions/ProductViewActions.js
+++ frontend/frontend-website/src/actions/ProductViewActions.js
@@ -12,10 +12,14 @@ export const getProduct = (id) => dispatch => {
 
     const payload = {
         product:
-            {
-                id: '1',
-                name: 'wiertarka',
-            }
+        {
+            Product_id: '3',
+            Name: 'Koszulka',
+            Category_Name: 'odzież',
+            User_Username: 'LeszekW',
+            Description: 'Wiara w konstytucję to najlepsze co możecie dla siebie zrobić',
+            Score: '10'
+        }
     };
 
     dispatch(getProductAction(payload.product));
@@ -28,9 +32,29 @@ export const getRecommendedProducts = (userId) => dispatch => {
     const payload = {
         products: [
             {
-                id: '1',
-                name: 'wiertarka',
+                Product_id: '9',
+                Name: 'Niegrzeczna Zosia',
+                Category_Name: 'ludzie',
+                User_Username: 'MamaZosi',
+                Description: 'Zosia była niegrzeczna więc oddam za darmo. Tylko odbiór osobisty, kontakt Priv.',
+                Score: '3'
             },
+            {
+                Product_id: '4',
+                Name: 'Kuper Programisty',
+                Category_Name: 'ludzie',
+                User_Username: 'xXxHTMLGodxXx',
+                Description: 'Nie polecam tego produktu pryszczaty, śmierdzi i czasem wychodzi brązowy płyn',
+                Score: '2'
+            },
+            {
+                Product_id: '3',
+                Name: 'Koszulka',
+                Category_Name: 'odzież',
+                User_Username: 'LeszekW',
+                Description: 'Wiara w konstytucję to najlepsze co możecie dla siebie zrobić',
+                Score: '10'
+            }
         ]
     };
 
