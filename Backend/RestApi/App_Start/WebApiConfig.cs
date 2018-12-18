c22372c8f99249a43ab5c8b81eadc036185f598a
P G <pgalewicz@gmail.com>
2018-12-18 17:45:27 +0100

    added recomendation controller to RestApi

--- Backend/RestApi/App_Start/WebApiConfig.cs
+++ Backend/RestApi/App_Start/WebApiConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using Newtonsoft.Json.Serialization;
 using RestApi.Filters;
@@ -25,9 +26,11 @@ namespace RestApi
             config.Filters.Add(new ExceptionLoggingFilter());
             
             config.MessageHandlers.Add(new RequestsLoggingHandler());
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
             
             JsonMediaTypeFormatter jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
             jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
         }
     }
 }
